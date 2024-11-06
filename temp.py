850ed0b270e3bf6e9c35b294057f2116a6a98cef
mazhenyu997 <mazhenyu997@foxmail.com>
2024-11-06 20:30:48 +0800

    model_2

---
+++ temp.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+# csv_file =pd.read_csv('./result/result.csv')
+#
+# test_output = csv_file[["id", "label"]]
+#
+# # 输出结果
+# test_output.to_csv('./result/temp.csv', index=None)
+#
+# csv_file = pd.read_csv('./test_/testa_x/testa_x.csv')
+# print(len(csv_file))
+print([i for i in range(100)])
